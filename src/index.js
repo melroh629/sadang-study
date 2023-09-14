a8a581db292d4c76d105d804129e302653095057
jungwoo <jw.roh629@gmail.com>
2023-09-14 19:08:16 +0900

    chore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+console.log('React version:', React.version)
+
 const root = document.getElementById('root')
 
 ReactDOM.createRoot(root).render(<h1>Hello world</h1>)
